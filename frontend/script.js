254c5081d4694f8bdd142a36a81fa8574e438533
Lucas <lucaseiki1@gmail.com>
2025-06-05 15:29:51 -0300

    começando o questionário

--- frontend/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".option").forEach(button => {
-  button.addEventListener("click", () => {
-    const value = button.dataset.value;
-    alert(`Você selecionou: ${value}`);
-  });
-});
